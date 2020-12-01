b368c5732fad1832a9fe35571eabf36e5e738c5d
Stephan Janosch <janosch@mpi-cbg.de>
2020-12-01 12:03:52 +0100

    adapted changes

--- wrapper.py
+++ wrapper.py
@@ -21,7 +21,7 @@ def main(argv):
 
         # 3. Call the image analysis workflow using the run script
         nj.job.update(progress=25, statusComment="Launching workflow...")
-        command = "/usr/bin/xvfb-run java -Xmx6000m -cp /fiji/jars/ij.jar ij.ImageJ --headless --console " \
+        command = "/usr/bin/xvfb-run java -Xmx1000m -cp /fiji/jars/ij.jar ij.ImageJ --headless --console " \
                   "-macro macro.ijm \"input={}, output={}, radius={}, threshold={}\"".format(in_path, out_path, nj.parameters.ij_radius, nj.parameters.ij_threshold)
         return_code = call(command, shell=True, cwd="/fiji")  # waits for the subprocess to return
 
